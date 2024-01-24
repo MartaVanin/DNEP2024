ccd3093d0fe57cd4173134bd4dc2076891e92467
Hakan Ergun <hakan.ergun@ieee.org>
2024-01-24 12:01:41 +0100

    adding time resolution into teime series creation, instead of time steps

--- examples/how_to.jl
+++ examples/how_to.jl
@@ -8,9 +8,15 @@ import PowerModelsDistribution as _PMD
 using Gurobi
 using Cbc
 
-data = _DNEP.get_33bus_network()
-mn_data = _DNEP.add_timeseries_33bus!(data; n_timesteps = 1000)
 solver = _PMD.optimizer_with_attributes(Gurobi.Optimizer, "TimeLimit" => 300) #"seconds" => 60)#, "TimeLimit" => 60)
+
+
+# 33 bus network
+data = _DNEP.get_33bus_network()
+mn_data = _DNEP.add_timeseries_33bus!(data; resolution = 60)
 res = _DNEP.solve_milp_dnep(mn_data, _PMD.LinDist3FlowPowerModel, solver, multinetwork = true)
 
+# irish network
 data_ir = _DNEP.get_irish_network()
+mn_data_ir = _DNEP.add_timeseries_irish!(data; resolution = 60)
+res = _DNEP.solve_milp_dnep(mn_data_ir, _PMD.LinDist3FlowPowerModel, solver, multinetwork = true)
--- src/io/add_timeseries.jl
+++ src/io/add_timeseries.jl
@@ -1,5 +1,4 @@
-
-function add_timeseries_33bus!(data::Dict; n_timesteps=527040)::Dict
+function add_timeseries_33bus!(data::Dict; resolution = 1)
     ts_gen = CSV.read(joinpath(BASE_DIR, "data/33Bus-Modified/Profiles/GenerationProfile.csv"), _DF.DataFrame, ntasks=1, header = 0) # unitless multipliers of rated generator size
     ts_dem = CSV.read(joinpath(BASE_DIR, "data/33Bus-Modified/Profiles/DemandProfiles.csv"), _DF.DataFrame, ntasks=1, header = 0)    # is in kW
     pf = CSV.read(joinpath(BASE_DIR, "data/33Bus-Modified/OpenDSS_Model/PowerFactors.csv"), _DF.DataFrame, ntasks=1, header = 0)     # power factors for the demand
@@ -18,21 +17,23 @@ function add_timeseries_33bus!(data::Dict; n_timesteps=527040)::Dict
                 "multinetwork" => true
             )
 
+    n_timesteps = Int(size(ts_gen,1) / resolution)      
     for t in 1:n_timesteps
+        t_idx = 1 + resolution * (t-1)
         mn_data["nw"]["$t"] = Dict{String, Any}()
         mn_data["nw"]["$t"] = deepcopy(data)
         for (l, load) in mn_data["nw"]["$t"]["load"]
             conns = load["connections"] # active load connections
             if haskey(load, "was_gen") # there is only one generation profile for all
-                load["pd"] = fill(-ts_gen[t, 1]*264.75/length(conns), length(conns)) ./ data["settings"]["sbase_default"] # generators all have size 264.75
+                load["pd"] = fill(-ts_gen[t_idx, 1]*264.75/length(conns), length(conns)) ./ data["settings"]["sbase_default"] # generators all have size 264.75
                 load["qd"] = fill(0., length(conns)) # generation always have PF = 1
             else
                 original_load_id = parse(Int, load["name"]) # as in openDSS. PMD scrambles the ids when it builds the dict
                 if original_load_id == 1
                     delete!(mn_data["nw"]["$t"]["load"], l) # in the .dss there is a load by the slackbus with always 0 power. confusing. let's just remove it
                 else
-                    load["pd"] = fill(ts_dem[t, original_load_id]/length(conns), length(conns)) ./ data["settings"]["sbase_default"] 
-                    load["qd"] = fill(ts_dem[t, original_load_id]/length(conns), length(conns)) ./ data["settings"]["sbase_default"] * tan(acos(pf[original_load_id, 1])) 
+                    load["pd"] = fill(ts_dem[t_idx, original_load_id]/length(conns), length(conns)) ./ data["settings"]["sbase_default"] 
+                    load["qd"] = fill(ts_dem[t_idx, original_load_id]/length(conns), length(conns)) ./ data["settings"]["sbase_default"] * tan(acos(pf[original_load_id, 1])) 
                 end
             end
         end
@@ -40,9 +41,9 @@ function add_timeseries_33bus!(data::Dict; n_timesteps=527040)::Dict
     return mn_data
 end
 
-function add_timeseries_irish!(data::Dict)
-    ts_gen = CSV.read(joinpath(BASE_DIR, "data/IrishMVGrid/Profiles/GenerationProfile.csv"), _DF.DataFrame, header = 0) # unitless multipliers of rated generator size
-    ts_dem = CSV.read(joinpath(BASE_DIR, "data/IrishMVGrid/Profiles/DemandProfiles.csv"), _DF.DataFrame, header = 0)    # unitless multipliers of rated load size
+function add_timeseries_irish!(data::Dict; resolution = 1)
+    ts_gen = CSV.read(joinpath(BASE_DIR, "data/IrishMVGrid/Profiles/GenerationProfileDingle.csv"), _DF.DataFrame, header = 0) # unitless multipliers of rated generator size
+    ts_dem = CSV.read(joinpath(BASE_DIR, "data/IrishMVGrid/Profiles/DemandProfileDingle.csv"), _DF.DataFrame, header = 0)    # unitless multipliers of rated load size
     load_info = CSV.read(joinpath(BASE_DIR, "data/IrishMVGrid/OpenDSS_Model/PowerFactors.csv"), _DF.DataFrame, header = 1) # rated P and Q for loads (power factor not really used). header = 1 is deliberate
 
     mn_data = Dict{String, Any}(
@@ -58,17 +59,19 @@ function add_timeseries_irish!(data::Dict)
                 "bus_lookup" => data["bus_lookup"]
             )
 
-    for t in 1:size(ts_gen)[1]
+    n_timesteps = Int(size(ts_gen,1) / resolution)  
+    for t in 1:n_time_steps
+        t_idx = 1 + resolution * (t-1)
         mn_data["nw"]["$t"] = Dict{String, Any}()
         mn_data["nw"]["$t"] = deepcopy(data)
         for (_, load) in mn_data["nw"]["$t"]["load"]
             if haskey(load, "was_gen")
-                load["pd"] = fill(-ts_gen[t, 1]/length(conns), length(conns)) ./ data["settings"]["sbase_default"]
+                load["pd"] = fill(-ts_gen[t_idx, 1]/length(conns), length(conns)) ./ data["settings"]["sbase_default"]
                 load["qd"] = fill(0., length(conns)) # generation always have PF = 1
             else
                 original_load_id = parse(Int, load["name"]) # as in openDSS. PMD scrambles the ids
-                load["pd"] = fill(ts_dem[t, original_load_id]/length(conns), length(conns)) ./ data["settings"]["sbase_default"] * load_info.kW[original_load_id]
-                load["qd"] = fill(ts_dem[t, original_load_id]/length(conns), length(conns)) ./ data["settings"]["sbase_default"] * load_info.kvar[original_load_id]
+                load["pd"] = fill(ts_dem[t_idx, original_load_id]/length(conns), length(conns)) ./ data["settings"]["sbase_default"] * load_info.kW[original_load_id]
+                load["qd"] = fill(ts_dem[t_idx, original_load_id]/length(conns), length(conns)) ./ data["settings"]["sbase_default"] * load_info.kvar[original_load_id]
             end
         end
     end
