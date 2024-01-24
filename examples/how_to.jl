7be2df16ce7869e8c39d181bcc76fcb1a24293db
Hakan Ergun <hakan.ergun@ieee.org>
2024-01-24 12:21:43 +0100

    fixing bugs

--- examples/how_to.jl
+++ examples/how_to.jl
@@ -13,10 +13,10 @@ solver = _PMD.optimizer_with_attributes(Gurobi.Optimizer, "TimeLimit" => 300) #"
 
 # 33 bus network
 data = _DNEP.get_33bus_network()
-mn_data = _DNEP.add_timeseries_33bus!(data; resolution = 3600)
+mn_data = _DNEP.add_timeseries_33bus!(data; resolution = 10000)
 res = _DNEP.solve_milp_dnep(mn_data, _PMD.LinDist3FlowPowerModel, solver, multinetwork = true)
 
 # irish network
 data_ir = _DNEP.get_irish_network()
-mn_data_ir = _DNEP.add_timeseries_irish!(data; resolution = 3600)
+mn_data_ir = _DNEP.add_timeseries_irish!(data; resolution = 10000)
 res = _DNEP.solve_milp_dnep(mn_data_ir, _PMD.LinDist3FlowPowerModel, solver, multinetwork = true)
--- src/io/add_timeseries.jl
+++ src/io/add_timeseries.jl
@@ -18,8 +18,10 @@ function add_timeseries_33bus!(data::Dict; resolution = 1)
             )
 
     n_timesteps = Int(round(size(ts_gen,1) / resolution))      
+    print(n_timesteps, "\n")
     for t in 1:n_timesteps
         t_idx = 1 + resolution * (t-1)
+        print(t_idx , "\n")
         mn_data["nw"]["$t"] = Dict{String, Any}()
         mn_data["nw"]["$t"] = deepcopy(data)
         for (l, load) in mn_data["nw"]["$t"]["load"]
@@ -59,7 +61,7 @@ function add_timeseries_irish!(data::Dict; resolution = 1)
                 "bus_lookup" => data["bus_lookup"]
             )
 
-            Int(round(size(ts_gen,1) / resolution)) 
+    n_timesteps = Int(round(size(ts_gen,1) / resolution)) 
     for t in 1:n_timesteps
         t_idx = 1 + resolution * (t-1)
         mn_data["nw"]["$t"] = Dict{String, Any}()
